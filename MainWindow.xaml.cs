ace768a09524b1f666369161e4126673f02ad82a
Michael Greene <VoltCruelerz@gmail.com>
2023-08-11 09:16:26 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Greed
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Debug.WriteLine("Main Window");
+            InitializeComponent();
+            Debug.WriteLine("Load Done");
+        }
+
+        private void Activate_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Activate_Click()");
+        }
+
+        private void Deactivate_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Deactivate_Click()");
+        }
+    }
+}
---
+++ Models/Metadata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Greed.Models
+{
+    class Metadata
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Version { get; set; }
+        public string Url { get; set; }
+        public string SinsVersion { get; set; }
+        public List<string> Dependencies { get; set; }// TODO
+        public List<string> Conflicts { get; set; }// TODO
+        public int Priority { get; set; }
+
+        public Metadata(string id, string json)
+        {
+            Id = id;
+            var obj = JObject.Parse(json);
+            Name = obj["name"].ToString();
+            Description = obj["description"].ToString();
+            Author = obj["author"].ToString();
+            Version = obj["version"].ToString();
+            Url = obj["url"].ToString();
+            SinsVersion = obj["sinsVersion"].ToString();
+            Dependencies = new List<string>();
+            Conflicts = new List<string>();
+        }
+    }
+}
