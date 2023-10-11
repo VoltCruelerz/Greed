23367cdaf60e0e1311bf287cb1b7887c398aa7ef
Michael Greene <VoltCruelerz@gmail.com>
2023-10-10 21:24:28 -0400

    better tests

--- Greed.UnitTest/Models/Mutations/OpConcatTest.cs
+++ Greed.UnitTest/Models/Mutations/OpConcatTest.cs
@@ -7,14 +7,14 @@ namespace Greed.UnitTest.Models.Mutations
     public class OpConcatTest
     {
         [TestMethod]
-        public void Concat_Shallow_Int()
+        public void Concat_Shallow_SingleArr_Int()
         {
             // Arrange
             var root = JObject.Parse("{ \"a\": [0, 1] }");
             var config = new JObject
             {
                 { "path", "a[i]" },
-                { "value", 3 }
+                { "value", 2 }
             };
             var op = new OpConcat(config);
 
@@ -22,11 +22,13 @@ namespace Greed.UnitTest.Models.Mutations
             op.Exec(root);
 
             // Assert
-            Assert.AreEqual(3, ((JArray)root["a"]!).Count);
+            var arr = (JArray)root["a"]!;
+            Assert.AreEqual(3, arr.Count);
+            Assert.AreEqual(2, arr[arr.Count - 1]);
         }
 
         [TestMethod]
-        public void Concat_Shallow_Str()
+        public void Concat_Shallow_SingleArr_Str()
         {
             // Arrange
             var root = new JObject
@@ -44,7 +46,33 @@ namespace Greed.UnitTest.Models.Mutations
             op.Exec(root);
 
             // Assert
-            Assert.AreEqual(3, ((JArray)root["a"]!).Count);
+            var arr = (JArray)root["a"]!;
+            Assert.AreEqual(3, arr.Count);
+            Assert.AreEqual("strC", arr[arr.Count - 1]);
+        }
+
+        [TestMethod]
+        public void Concat_Shallow_DoubleArr_Str()
+        {
+            // Arrange
+            var root = new JObject
+            {
+                { "a", new JArray { new JArray { "strA", "strB" }, new JArray { "strC", "strD" } } }
+            };
+            var config = new JObject
+            {
+                { "path", "a[i][j]" },
+                { "value", new JArray { "strA", "strB" } }
+            };
+            var op = new OpConcat(config);
+
+            // Act
+            op.Exec(root);
+
+            // Assert
+            var arr = (JArray)root["a"]!;
+            Assert.AreEqual(3, arr.Count);
+            Assert.AreEqual(2, ((JArray)arr[arr.Count - 1]).Count);
         }
     }
 }
